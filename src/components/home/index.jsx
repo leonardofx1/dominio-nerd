1c8ad8b22fca49f85c3ae7d2098b1aefc73cfe4b
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-04-04 22:42:09 -0300

    Ajustando espaço de textos e imagens com botões

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 
 import * as C from './styles'
+import MiniBotton from '/mini-wonderwoman.svg'
+import MiniTop from '/mini-thor.svg'
 
 const Home = () => {
     return (
         <C.Main>
+            <C.Mini
+                src={MiniBotton}
+                alt='Miniatura da Mulher Maravilha'
+                style={{
+                    bottom:-10,
+                    right:375
+                }}
+            />
+            <C.Mini
+                src={MiniTop}
+                alt='Miniatura do Thor'
+                style={{
+                    right:10,
+                    top:-40
+                }}
+            />
             <C.Container>
-                <h1>teste 1</h1>
+                <C.TextField>
+                    <h1 style={{marginBottom:'1rem'}}>Dominio Nerd</h1>
+                    <h2 style={{marginBottom:'1rem'}}>Venha jogar Heroclix!</h2>
+                    <p style={{marginBottom:'2rem'}}>Nesse jogo, acontece e então as coisas serão jogadas com regras e detalhes para  melhor quando então foi assim que se fez e com isso o melhor de tudo que pode e vença desafios.</p>
+                    <button>Saiba mais</button>
+                </C.TextField>
             </C.Container>
             <C.Container>
+                <button style={{zIndex:5}}>Prev</button>
                 <C.SlideContainer>
                     <C.SlideBox>
 
@@ -20,6 +44,7 @@ const Home = () => {
                         
                     </C.SlideBox>
                 </C.SlideContainer>
+                <button style={{zIndex:5}}>Next</button>
             </C.Container>
         </C.Main>
     )
--- src/components/home/styles.jsx
+++ src/components/home/styles.jsx
@@ -7,17 +7,29 @@ export const Main = styled.main`
     background-size: cover;
     display: flex;
     min-height: calc(100vh - 100px);
+    position: relative;
     width: 100vw;
 `
 
 export const Container = styled.div`
     align-items: center;
+    background: #00000075;
     color: var(--cor-light);
     display: flex;
     justify-content: center;
     min-width: 50%;
     overflow: hidden;
 `
+export const TextField = styled.div`
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+    top: 80px;
+    text-align: center;
+    width: 40%;
+`
+
 export const SlideContainer = styled.div`
     align-items: center;
     display: flex;
@@ -32,4 +44,9 @@ export const SlideBox = styled.div`
     margin: 0 1rem;
     min-height: calc(90vh - 100px);
     min-width: 30vw;
+`
+
+export const Mini = styled.img`
+    position: absolute;
+    width: 300px
 `
