d4c27ca3626ec4fc0c0e17cafddeae0dfb807be8
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-04-04 21:29:06 -0300

    divs pro slide da Home

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -5,7 +5,22 @@ import * as C from './styles'
 const Home = () => {
     return (
         <C.Main>
-            
+            <C.Container>
+                <h1>teste 1</h1>
+            </C.Container>
+            <C.Container>
+                <C.SlideContainer>
+                    <C.SlideBox>
+
+                    </C.SlideBox>
+                    <C.SlideBox>
+                        
+                    </C.SlideBox>
+                    <C.SlideBox>
+                        
+                    </C.SlideBox>
+                </C.SlideContainer>
+            </C.Container>
         </C.Main>
     )
 }
--- src/components/home/styles.jsx
+++ src/components/home/styles.jsx
@@ -5,6 +5,31 @@ import backgroundPage from '/hq.bg01.png'
 export const Main = styled.main`
     background: url(${backgroundPage});
     background-size: cover;
+    display: flex;
     min-height: calc(100vh - 100px);
     width: 100vw;
+`
+
+export const Container = styled.div`
+    align-items: center;
+    color: var(--cor-light);
+    display: flex;
+    justify-content: center;
+    min-width: 50%;
+    overflow: hidden;
+`
+export const SlideContainer = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    width: 65%;
+    overflow: hidden;
+`
+
+export const SlideBox = styled.div`
+    background: var(--cor-light);
+    border-radius: 15px;
+    margin: 0 1rem;
+    min-height: calc(90vh - 100px);
+    min-width: 30vw;
 `
