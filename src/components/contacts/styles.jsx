d423377fc9e9cf9747009c4041e3912a5c4eb995
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-04-08 13:58:21 -0300

    Abrindo componente Contacts

---
+++ src/components/contacts/styles.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    background: var(--cor-light);
+    min-height: calc(100vh - 100px);
+    min-width: 100vw;
+`
+
+export const TopBackgroundField = styled.div`
+    
+
+`
+
+export const InfosField = styled.div`
+
+`
+
+export const AdressField = styled.div`
+    
+`
+
+export const SocialMediaField = styled.div`
+    
+`
