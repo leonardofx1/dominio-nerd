727ff8ae175a0b458cdff808bf7d96122f3abce0
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-05-04 17:53:56 -0300

    Preenchendo base de conteudos  por areas de links

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
+import { 
+    RiWhatsappFill,
+    RiInstagramLine,
+    RiPhoneFill,
+    RiMailLine
+} from 'react-icons/ri'
 
 import * as C from './styles'
 
-const Footer = () =>{
+const Footer = () => {
     return(
-        <div></div>
+        <C.Footer>
+            <C.AboutUs>
+                <h2>About Us</h2>
+                <p>
+                    SADLAJSLASJAS
+                </p>
+                <ul>
+                    <li><a href='#'><RiWhatsappFill/></a></li>
+                    <li><a href='#'><RiInstagramLine/></a></li>
+                </ul>
+            </C.AboutUs>
+            <C.QuickLinks>
+                <h2>Support</h2>
+                <ul>
+                    <li><a href='#'>FAQ</a></li>
+                    <li><a href='#'>Privacy Policy</a></li>
+                    <li><a href='#'>Help</a></li>
+                    <li><a href='#'>Contact</a></li>
+                </ul>
+            </C.QuickLinks>
+            <C.QuickLinks>
+            <h2>Menu</h2>
+                <ul>
+                    <li><a href='#'>Início</a></li>
+                    <li><a href='#'>Produtos</a></li>
+                    <li><a href='#'>Regras Rápidas</a></li>
+                    <li><a href='#'>Calendário de jogos</a></li>
+                </ul>
+            </C.QuickLinks>
+            <C.Contact>
+                <h2>Contatos</h2>
+                <ul>
+                    <li><a href='tel:+5522999424224'><RiPhoneFill/> +55 22 99543-5269</a></li>
+                    <li><a href='mailto:marcio.b.a.dias@gmail.com'><RiMailLine/> contato@dominionerd.com</a></li>
+                </ul>
+            </C.Contact>
+        </C.Footer>
     )
 }
 
