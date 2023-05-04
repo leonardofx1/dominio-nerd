f6a72deaa053ca9071da432b5f07fbc184bd7f30
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-05-04 19:46:18 -0300

    Ajustando CSS

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -14,43 +14,44 @@ const Footer = () => {
         <C.Container>
         <C.Footer>
             <C.AboutUs>
-                <h2>About Us</h2>
+                <C.Title>About Us</C.Title>
                 <p>
-                    SADLAJSLASJAS
+                Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
                 </p>
-                <ul>
-                    <li><a href='#'><RiWhatsappFill/></a></li>
-                    <li><a href='#'><RiInstagramLine/></a></li>
-                </ul>
+                <C.List>
+                    <C.ListItem><C.ALink href='#'><RiWhatsappFill/></C.ALink></C.ListItem>
+                    <C.ListItem><C.ALink href='#'><RiInstagramLine/></C.ALink></C.ListItem>
+                </C.List>
             </C.AboutUs>
             <C.QuickLinks>
-                <h2>Support</h2>
-                <ul>
-                    <li><a href='#'>FAQ</a></li>
-                    <li><a href='#'>Privacy Policy</a></li>
-                    <li><a href='#'>Help</a></li>
-                    <li><a href='#'>Contact</a></li>
-                </ul>
+                <C.Title>Support</C.Title>
+                <C.List>
+                    <C.ListItem><C.ALink href='#'>FAQ</C.ALink></C.ListItem>
+                    <C.ListItem><C.ALink href='#'>Privacy Policy</C.ALink></C.ListItem>
+                    <C.ListItem><C.ALink href='#'>Help</C.ALink></C.ListItem>
+                    <C.ListItem><C.ALink href='#'>Contact</C.ALink></C.ListItem>
+                </C.List>
             </C.QuickLinks>
             <C.QuickLinks>
-            <h2>Menu</h2>
-                <ul>
-                    <li><a href='#'>Início</a></li>
-                    <li><a href='#'>Produtos</a></li>
-                    <li><a href='#'>Regras Rápidas</a></li>
-                    <li><a href='#'>Calendário de jogos</a></li>
-                </ul>
+            <C.Title>Menu</C.Title>
+                <C.List>
+                    <C.ListItem><C.ALink href='#'>Início</C.ALink></C.ListItem>
+                    <C.ListItem><C.ALink href='#'>Produtos</C.ALink></C.ListItem>
+                    <C.ListItem><C.ALink href='#'>Regras Rápidas</C.ALink></C.ListItem>
+                    <C.ListItem><C.ALink href='#'>alendário de jogos</C.ALink></C.ListItem>
+                </C.List>
             </C.QuickLinks>
             <C.Contact>
-                <h2>Contatos</h2>
-                <ul>
-                    <li><a href='tel:+5522999424224'><RiPhoneFill/> +55 22 99543-5269</a></li>
-                    <li><a href='mailto:marcio.b.a.dias@gmail.com'><RiMailLine/> contato@dominionerd.com</a></li>
-                </ul>
+                <C.Title>Contatos</C.Title>
+                <C.List>
+                    <C.ListItem><C.ALink href='tel:+5522999424224'><RiPhoneFill/> +5522 99543-5269</C.ALink></C.ListItem>
+                    <C.ListItem><C.ALink href='mailto:marcio.b.a.dias@gmail.com'><RiMailLine/>contato@dominionerd.com</C.ALink></C.ListItem>
+                </C.List>
             </C.Contact>
         </C.Footer>
         <C.CopyRightText>
-            <p><RiCopyrightLine/>Marcio Dias. Para informações e orçamentos: <a href='#'><RiWhatsappFill/></a> <a href='#'><RiInstagramLine/></a>
+            <p><RiCopyrightLine/>Marcio Dias. Para informações e orçamentos: <C.ALink href='#'><RiWhatsappFill/></C.ALink> <C.ALink href='#'><RiInstagramLine/></C.ALink>
             </p>
         </C.CopyRightText>
         </C.Container>
--- src/components/footer/styles.jsx
+++ src/components/footer/styles.jsx
@@ -1,13 +1,64 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+    display: grid;
+    justify-content: end;
+    align-items: end;
+    min-height: 30vh;
+    background: var(--cor-gray);
+    color: var(--cor-yellow);
+    padding: 5rem;
+`
 
-export const Footer = styled.footer``
-
+export const Footer = styled.footer`
+    position: relative;
+    width: 100%;
+    height: auto;
+    padding: 5rem, 10rem;
+    display: grid;
+    grid-template-columns: 2fr 1fr 1fr 1fr;
+    grid-gap: 2rem;
+`
 export const AboutUs = styled.div``
 
-export const QuickLinks = styled.div``
+export const QuickLinks = styled.div`
+    position: relative;
+    display: inline-block;
+`
 
-export const Contact = styled.div``
+export const Contact = styled.div`
+    position: relative;
+    display: inline-block;
+`
 
 export const CopyRightText = styled.div``
+
+export const Title = styled.h2`
+    position: relative;
+    margin-bottom: 1rem;
+`
+
+export const List = styled.ul`
+    margin-top: 2rem;
+    display: grid;
+
+    
+`
+
+export const ListItem = styled.li`
+    list-style: none;
+    margin-bottom: 1rem;
+`
+
+export const ALink = styled.a`
+    color: var(--cor-yellow);
+
+    svg{
+        width: 36px;
+        height: 36px;
+    }
+    
+    :hover{
+        color: var(--cor-light);
+    }
+`
