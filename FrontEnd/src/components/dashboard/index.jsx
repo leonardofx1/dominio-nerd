6f942c083258f27290eeb405cefeb39000728d0b
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-05-10 15:21:24 -0300

    repaginando a arquitetura de rotas do dashboard

--- FrontEnd/src/components/dashboard/index.jsx
+++ FrontEnd/src/components/dashboard/index.jsx
@@ -1,48 +1,23 @@
 import React from 'react'
 
+import { Routes, Route } from 'react-router-dom'
+
+import MenuDashboard from './menu-dashboard'
+import RegisterProducts from './register-products'
+import PedingOrders from './peding-orders'
+
 import * as C from './styles'
-import { Field, Form, Formik } from 'formik'
 
 const Dashboard = () => {
     return(
         <C.Main>
-            <C.MenuContainer>
-                <C.List>
-                    <C.ItemList>Cadastrar de Produtos</C.ItemList>
-                    <C.ItemList>Pedidos Pendentes</C.ItemList>
-                    <C.ItemList>Estoque</C.ItemList>
-                    <C.ItemList>Histórico de Vendas</C.ItemList>
-                </C.List>
-            </C.MenuContainer>
-
-            <C.DashboardField>
-                <C.Title>Cadastro de produtos</C.Title>
-
-                <C.Container>
-                <h2>
-                    Insira as informações baixo para cadastrar um novo produto:
-                </h2>
-                <Formik>
-                    <Form>
-                        <C.FieldContainer>
-                            <span>Nome do Produto:</span>
-                            <Field />
-                            <span>Edição:</span>
-                            <Field />
-                            <span>Url de imagem do produto:</span>
-                            <Field />
-                            <span>Estoque:</span>
-                            <Field />
-                            <span>Valor:</span>
-                            <Field />
-                        </C.FieldContainer>
-                        <button>Cadastrar</button>
-                    </Form>
-                </Formik>
-            </C.Container>
-
-            </C.DashboardField>
+            <MenuDashboard/>
+            <Routes>
+            <Route path='/dashboard/newProduct' element={<RegisterProducts/>} />
+            <Route path='/dashboard/pending-orders' element={<PedingOrders/>} />
+            </Routes>
         </C.Main>
+
     )
 }
 
